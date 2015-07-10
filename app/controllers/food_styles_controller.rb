c9c0f4c09beaa3d71287c8572ddd2f3fed70fcca
Brian Newton <notthemessiah63@gmail.com>
2015-07-10 11:39:18 +0100

    added show page for foods

--- app/controllers/food_styles_controller.rb
+++ app/controllers/food_styles_controller.rb
@@ -1,2 +1,14 @@
 class FoodStylesController < ApplicationController
+  def index
+    @food_styles = FoodStyle.all
+    respond_to do |format|
+      format.html
+      format.json
+      @food_style = FoodStyle.new
+    end
+  end
+
+  def show
+    @food_style = FoodStyle.find(params[:id])
+  end
 end
