277afb58a017eeaec12ad606870065a220c8727a
Brian Newton <notthemessiah63@gmail.com>
2015-07-11 09:48:19 +0100

    developed style_matches controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,28 @@
 class MatchesController < ApplicationController
+  def index
+    @matches = Match.all
+    respond_to do |format|
+      format.html
+      format.json
+      @match = Match.new
+    end
+  end
+
+  def show
+    @match = Match.find(params[:id])
+  end
+
+  def create
+    match = Match.create params[:match].permit(:match_note, :beer_id, :review_by, :review_date, :review_val, :food_id)
+    if match.save
+      redirect_to foods_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def match_params
+    params.require(:match).permit(:match_note, :beer_id, :review_by, :review_date, :review_val, :food_id)
+  end
 end
--- app/controllers/style_matches_controller.rb
+++ app/controllers/style_matches_controller.rb
@@ -1,2 +1,28 @@
 class StyleMatchesController < ApplicationController
+    def index
+    @style_matches = StyleMatch.all
+    respond_to do |format|
+      format.html
+      format.json
+      @style_match = StyleMatch.new
+    end
+  end
+
+  def show
+    @style_match = StyleMatch.find(params[:id])
+  end
+
+  def create
+    match = StyleMatch.create params[:style_match].permit(:match_note, :beer_style_id, :food_style_id)
+    if match.save
+      redirect_to foods_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def match_params
+    params.require(:match).permit(:match_note, :beer_style_id, :food_style_id)
+  end
 end
