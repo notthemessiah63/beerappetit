cf95e4bbc97dc52be1b5ceae2eb52fb78132e8f8
Brian Newton <notthemessiah63@gmail.com>
2015-07-14 15:58:44 +0100

    added helper 'beer_matches_details' to get avg rating for specific beer

--- app/helpers/foods_helper.rb
+++ app/helpers/foods_helper.rb
@@ -1,2 +1,16 @@
 module FoodsHelper
+  def beer_matches_details(beer)
+    count = []
+    html = '' 
+    beer.matches.each do |match|
+      if match.food_id == @food.id 
+        count.push(match.review_val) if match.review_val 
+          html += "<p>#{match.match_note} </p>"
+          html += "<p>#{match.review_val}</p>"
+      end 
+    end
+    count_total = (count.inject(:+)) / (count.length) if count.length > 0
+    [count_total, html] 
+  end
+
 end
